0f5b15c343e5eaa7d29c976b3a0c7b6f36c23a33
mir4oni3 <m.stoyanovski3@gmail.com>
2023-05-15 14:04:41 +0300

    Utility functions

---
+++ Utils.cpp
@@ -0,0 +1,39 @@
+#include "Utils.h"
+#include "Constants.h"
+#include <cstring>
+
+bool isInAlphabet(char ch) {
+	for (int i = 0; i < strlen(alphabet); i++) {
+		if (alphabet[i] == ch) {
+			return true;
+		}
+	}
+	return false;
+}
+
+MyString toString(const MyStack<char> stack) {
+	MyStack<char> st = stack;
+	char* str = new char[2 * st.getCount()];
+	int i = 0;
+	while (!st.isEmpty()) {
+		str[i++] = ' ';
+		str[i++] =st.pop();
+	}
+	MyString result(str);
+	delete[] str;
+	return result;
+}
+
+MyString toString(const MyQueue<char> queue) {
+	MyQueue<char> q = queue;
+	char* str = new char[2 * q.getCount()];
+	int i = 0;
+	int i = 0;
+	while (!q.isEmpty()) {
+		str[i++] = ' ';
+		str[i++] = q.dequeue();
+	}
+	MyString result(str);
+	delete[] str;
+	return result;
+}
---
+++ Utils.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "MyStack.h"
+#include "MyQueue.h"
+
+bool isInAlphabet(char ch);
+MyString toString(const MyStack<char> st);
+MyString toString(const MyQueue<char> st);
