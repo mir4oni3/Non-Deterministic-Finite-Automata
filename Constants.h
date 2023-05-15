77749409b4d4520c4e996810045629b612b97853
mir4oni3 <m.stoyanovski3@gmail.com>
2023-05-15 14:03:21 +0300

    Constants
    Some constants. Feel free to modify them.

---
+++ Constants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+static const char EPSILON = 'E';
+static const char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
+								'n','o','p','q','r','s','t','u','v','w','x','y','z',
+								'0','1','2','3','4','5','6','7','8','9'};
+
+static const size_t initialDataStructureCapacity = 8;
+static const size_t minStateCapacity = 8;
+static const size_t minTransitionCapacity = 16;
